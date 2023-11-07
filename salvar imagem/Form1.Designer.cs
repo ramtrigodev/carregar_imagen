a18b0e4ab569406d04fe67a02f6127de46cb190a
Ramon Trigo <ramtrigo@gmail.com>
2023-11-07 13:39:28 -0300

    projeto

---
+++ salvar imagem/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace salvar_imagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picturebox = new System.Windows.Forms.PictureBox();
+            this.carregarfoto = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picturebox
+            // 
+            this.picturebox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picturebox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picturebox.Location = new System.Drawing.Point(12, 12);
+            this.picturebox.Name = "picturebox";
+            this.picturebox.Size = new System.Drawing.Size(140, 179);
+            this.picturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturebox.TabIndex = 37;
+            this.picturebox.TabStop = false;
+            this.picturebox.Click += new System.EventHandler(this.ptBImagem_Click);
+            // 
+            // carregarfoto
+            // 
+            this.carregarfoto.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 306);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(114, 44);
+            this.button2.TabIndex = 55;
+            this.button2.Text = "Savar Cadastro";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 362);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.picturebox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picturebox;
+        private System.Windows.Forms.OpenFileDialog carregarfoto;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ salvar imagem/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace salvar_imagem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string arquivo, caminhoarquivo;
+        private void btnAddImagem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+             
+            
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            picturebox.Image.Save("D:\\fotos\\" + arquivo + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            string dbfile = carregarfoto.FileName;
+
+
+        }
+
+        private void ptBImagem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                carregarfoto.ShowDialog();
+                Bitmap bmp = new Bitmap(carregarfoto.FileName);
+
+                Bitmap bmp2 = new Bitmap(bmp);
+                picturebox.Image = bmp2;
+                string nomeArquivo = Path.GetFileNameWithoutExtension(carregarfoto.FileName);
+
+                arquivo = nomeArquivo;
+                
+                caminhoarquivo = ("D:\\fotos\\" + arquivo + ".png");
+              i._caminho = caminhoarquivo.Replace(@"\", @"\\"); ;
+                   i.inserir();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar.");
+            }
+
+        }
+    }
+    }
+
+
+
+
